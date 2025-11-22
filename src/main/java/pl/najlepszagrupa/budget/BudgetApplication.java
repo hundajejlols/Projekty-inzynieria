21e1129dbac5a4b2b94af2f05270962f226f70ac
Mateusz1218 <mateusz.janusz@upsanok.onmicrosoft.com>
2025-11-22 22:39:10 +0100

    Added Spring Web

--- src/main/java/pl/najlepszagrupa/budget/BudgetApplication.java
+++ src/main/java/pl/najlepszagrupa/budget/BudgetApplication.java
@@ -2,12 +2,14 @@ package pl.najlepszagrupa.budget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BudgetApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BudgetApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(BudgetApplication.class, args);}
+    @GetMapping("/home")
+    public String home() {return "Hello World!";}
 
 }
