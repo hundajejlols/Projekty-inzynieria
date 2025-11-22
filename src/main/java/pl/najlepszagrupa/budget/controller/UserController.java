fcf29ff7662657acf634c8241a182eeb370bbea4
Mateusz1218 <mateusz.janusz@upsanok.onmicrosoft.com>
2025-11-22 22:46:59 +0100

    Added Springboot Dev Tools

---
+++ src/main/java/pl/najlepszagrupa/budget/controller/UserController.java
@@ -0,0 +1,10 @@
+package pl.najlepszagrupa.budget.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/users")
+    public String getUsers() {return "Lista Użytkowników";}
+}
